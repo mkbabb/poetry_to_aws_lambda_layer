204589fa5ba1d338e446feb65b408d189fb0bb62
Mike Babb <mike7400@gmail.com>
2022-07-06 16:27:35 -0400

    Added outputs

--- main.tf
+++ main.tf
@@ -1,15 +1,19 @@
+terraform {
+  required_version = ">= 0.13"
+}
+
 provider "aws" {
   region = "us-east-1"
 }
 
 
-resource "null_resource" "zip_lambda_layer" {
+resource "null_resource" "zip_layer" {
   provisioner "local-exec" {
     command = "${path.module}/build.sh ${var.dist_path}"
   }
 }
 
-resource "aws_lambda_layer_version" "lambda_layer" {
+resource "aws_lambda_layer_version" "layer" {
   filename   = "${var.dist_path}/layer.zip"
   layer_name = var.layer_name
 
---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "arn" {
+  description = "ARN of the Layer"
+  value       = aws_lambda_layer_version.layer.arn
+}
+
