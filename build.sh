dc37f3edf28e2990b834a5d04b31acdd89e072a3
Mike Babb <mike7400@gmail.com>
2022-07-06 16:21:19 -0400

    Added rm

--- build.sh
+++ build.sh
@@ -3,8 +3,10 @@ DIST="${1:-./dist}"
 
 mkdir -p $DIST/python
 
-poetry export -f requirements.txt --without-hashes >requirements.txt
-
-python3 -m pip install -r requirements.txt -t $DIST/python
+# Export the packages from poetry; install locally.
+# No devevelopment dependencies.
+poetry export -f requirements.txt --without-hashes >$DIST/requirements.txt
+python3 -m pip install $DIST/requirements.txt -t $DIST/python
+rm $DIST/requirements.txt
 
 zip -r $DIST/layer.zip $DIST/python
