0781eb3eccd21f19e07c14431b0fe6b55bf9688a
Mike Babb <mike7400@gmail.com>
2022-08-12 03:44:43 -0400

    Changed pip

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ cp $2 $DIST/python
 # Export the packages from poetry; install locally.
 # No devevelopment dependencies.
 poetry export -f requirements.txt --without-hashes >$DIST/requirements.txt
-python3 -m pip install -r $DIST/requirements.txt -t $DIST/python
+python3 -m pip install --platform=manylinux1_x86_64 --only-binary=:all: -r $DIST/requirements.txt -t $DIST/python
 rm $DIST/requirements.txt
 
 zip -r $DIST/layer.zip $DIST/python
