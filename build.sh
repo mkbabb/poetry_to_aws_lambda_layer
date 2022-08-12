d1412ab8db74bd773727933084e861306d897cb8
Mike Babb <mike7400@gmail.com>
2022-08-12 03:52:21 -0400

    tmp

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/zsh
 DIST="${1:-./dist}"
 
+PYTHON_VERSION=3.9
+
 mkdir -p $DIST/python
 
 cp $2 $DIST/python
@@ -8,7 +10,7 @@ cp $2 $DIST/python
 # Export the packages from poetry; install locally.
 # No devevelopment dependencies.
 poetry export -f requirements.txt --without-hashes >$DIST/requirements.txt
-python3 -m pip install --platform=manylinux1_x86_64 -r $DIST/requirements.txt -t $DIST/python
+python3 -m pip install --platform=manylinux1_x86_64 --only-binary=:all: --python-version $PYTHON_VERSION -r $DIST/requirements.txt -t $DIST/python
 rm $DIST/requirements.txt
 
 zip -r $DIST/layer.zip $DIST/python
