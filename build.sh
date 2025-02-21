83da173cf5111dffbdee83e1663b6e0b4e0b63d9
Mike Babb <mike7400@gmail.com>
2025-02-21 14:11:41 -0500

    extra paths fix

--- build.sh
+++ build.sh
@@ -2,14 +2,15 @@
 
 # Default to Python 3.12
 PYTHON_VERSION="${1:-3.12}"
+
 # Default to ./dist
 DIST="${2:-./dist}"
 
+# Extra paths to include in the dist folder; args 3-n
+EXTRA_PATHS="${@:3}"
+
 # Create the dist folder
 mkdir -p $DIST/python
-
-# Copy the "extra_paths" into the dist folder.
-cp $3 $DIST/python
 
 # Export the packages from poetry; install locally.
 
@@ -18,6 +19,6 @@ poetry export -f requirements.txt --without-hashes >$DIST/requirements.txt
 poetry run python3 -m pip install --platform=manylinux1_x86_64 --no-deps --python-version $PYTHON_VERSION -r $DIST/requirements.txt -t $DIST/python
 
 cd $DIST
-zip -r layer.zip python/
+zip -r layer.zip python/ $EXTRA_PATHS
 
 rm -rf python requirements.txt
