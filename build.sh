991da8cdfb323eadc38a195203d8d7d105c82e4e
Mike Babb <mike7400@gmail.com>
2025-02-21 13:18:49 -0500

    tmp

--- build.sh
+++ build.sh
@@ -5,12 +5,14 @@ PYTHON_VERSION="${1:-3.12}"
 # Default to ./dist
 DIST="${2:-./dist}"
 
-mkdir $DIST/python
+# Create the dist folder
+mkdir -p $DIST/python
 
 # Copy the "extra_paths" into the dist folder.
 cp $3 $DIST/python
 
 # Export the packages from poetry; install locally.
+
 # No devevelopment dependencies.
 poetry export -f requirements.txt --without-hashes >$DIST/requirements.txt
 python3 -m pip install --platform=manylinux1_x86_64 --no-deps --python-version $PYTHON_VERSION -r $DIST/requirements.txt -t $DIST/python
